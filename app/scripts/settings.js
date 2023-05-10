ca8189d2ac03056e96ac2c93c636e5e30f581f36
JulieSampieri <118740189+JulieSampieri@users.noreply.github.com>
2023-05-10 14:31:52 +0200

    Ajout des variables

--- app/scripts/settings.js
+++ app/scripts/settings.js
@@ -2,8 +2,13 @@
 (function() {
   'use strict';
   angular.module('appSettings', []).constant('SETTINGS', {
-    API_DOMAIN: 'http://localhost:8080',
-    FRONT_DOMAIN: 'http://localhost:9000',
+    API_DOMAIN: 'https://vigiechiro-test.herokuapp.com',
+    FRONT_DOMAIN: 'https://vigiechiro-dev.in2p3.fr',
+    GOOGLE_ANALYTICS_ID: 'UA-61383739-1',
+    S3_BUCKET_URL: 'https://vigiechiro.s3.amazonaws.com/',
+    S3_BUCKET_NAME: 'vigiechiro-test',
+    S3_REGION: 'eu-west-1',
+    GOOGLE_MAPS_API_KEY: 'AIzaSyCuxbBluKEqE-kJLmxhY0MHB0kSxMMkCAQ', 
     BASE_TITLE: 'Vigiechiro'
   });
 
